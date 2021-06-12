0606afab67919d36b8262be62bed17e8eca6ab8a
Watson-Sei <55475145+Watson-Sei@users.noreply.github.com>
2021-06-12 11:29:53 +0900

    Added the design and implementation of the input tag.

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,12 @@
   <div class="container">
     <div class="Side">Side</div>
     <div class="Content">Content</div>
-    <div class="Form">Form</div>
+    <div class="Form">
+      <div class="form-content">
+        <input v-model="message" placeholder="ENTERキーで送信(上限800文字)" maxlength="800" />
+        <button @click="sendChat">送信</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,7 +37,7 @@ export default {
   display: grid;
   height: 100vh;
   grid-template-rows: 1fr 100px;
-  grid-template-columns: 350px 1fr;
+  grid-template-columns: 300px 1fr;
 }
 .Side {
   grid-row: 1 / 3;
@@ -48,5 +53,19 @@ export default {
   grid-row: 2 / 3;
   grid-column: 2 / 3;
   background: #88f;
+  display: table;
+  width: 100%;
+}
+.form-content {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
+.form-content input {
+  width: 70%;
+  height: 40px;
+}
+.form-content button {
+  height: 40px;
 }
 </style>
